acba5a27af3d50b20cde7b35eba29b814c12b82b
ThomasBackers <thomasbackers@gmail.com>
2021-11-19 21:11:20 +0100

    began to code the break  modal

--- src/app/App.js
+++ src/app/App.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Header from './components/Header'
 import Clock from './components/Clock'
 import Footer from './components/Footer'
-import './styles/main.scss'
 
 const App = () => {
   return (
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app/App'
+import './app/styles/main.scss'
 
 ReactDOM.render(
   <React.StrictMode>
